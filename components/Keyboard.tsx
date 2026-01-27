2ba667d5d23404c5b0d6341020f532dd2e262329
GBBrunner <garrett.brunner3090@mtec.stu.edu>
2026-01-27 13:43:50 -0700

    s

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -4,7 +4,7 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 const ROWS = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["A change v5", "Z", "X", "C", "V", "B", "N", "M", "DEL"],
+  ["A change v6", "Z", "X", "C", "V", "B", "N", "M", "DEL"],
 ];
 
 type Props = {
